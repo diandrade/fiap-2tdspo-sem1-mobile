bb0bf19a3b7375be06e20ef91d4fcfeb03e1ad01
diandrade <contato.andradediego@gmail.com>
2026-02-12 22:02:43 -0300

    feat: Adição do exercício 01.

---
+++ EX01/01.js
@@ -0,0 +1,8 @@
+//Demonstração
+console.log("Olá, mundo! Este é meu primeiro script.");
+console.log("Aprendendo JavaScript com React Native.");
+
+//Exercício
+console.log("Meu nome é Diego.");
+console.log("Estou cursando Análise e Desenvolvimento de Sistemas.");
+console.log("Estou aprendendo JavaScript!");
