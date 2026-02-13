116ea7e2a06287bc18e7fbe506c781098d0c9a16
diandrade <contato.andradediego@gmail.com>
2026-02-12 22:23:14 -0300

    feat: Conclusão do EX02.

---
+++ EX02/01.js
@@ -0,0 +1,5 @@
+const nomeJogador = "Ana";
+let pontuacao = 0;
+console.log ("A jogadora " + nomeJogador + " possui " + pontuacao + " pontos.");
+pontuacao = 10;
+console.log ("A jogadora " + nomeJogador + " possui " + pontuacao + " pontos.");
---
+++ EX02/02.js
@@ -0,0 +1,19 @@
+//Demonstração
+let cidade = "São Paulo";
+let populacao = 12300000;
+const ativa = true;
+console.log(typeof cidade);     // "string"
+console.log(typeof populacao);  // "number"
+console.log(typeof ativa);      // "boolean"
+
+//Exercício
+const nome = "Diego";
+const idade = 23;
+let estaMatriculadoNaFiap = true;
+let iraFinalizarOCurso = undefined;
+
+console.log(nome + " é do tipo: " + typeof(nome));
+console.log(idade + " é do tipo: " + typeof(idade));
+console.log(estaMatriculadoNaFiap + " é do tipo: " + typeof(estaMatriculadoNaFiap));
+console.log(iraFinalizarOCurso + " é do tipo: " + typeof(iraFinalizarOCurso));
+
