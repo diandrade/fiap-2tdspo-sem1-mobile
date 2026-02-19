86ecc01c1a3ee977f202767ecb7f1be2f26eb6ba
diandrade <contato.andradediego@gmail.com>
2026-02-19 18:48:35 -0300

    feat: EX06 Finalizado.

---
+++ EX06/01.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 10; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+let alunos = ["Ana", "Bruno", "Clara", "Diego"];
+for (let i = 0; i < alunos.length; i++){
+    console.log(i + " : " + alunos[i]);
+}
---
+++ EX06/02.js
@@ -0,0 +1,8 @@
+let contagem = 10;
+
+while(contagem > 0){
+    console.log(contagem + ", ");
+    contagem--;
+}
+
+console.log("🚀 Lançamento!");
