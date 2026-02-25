c60e237114453541cfd21bec06c767134320250a
diandrade <contato.andradediego@gmail.com>
2026-02-25 20:29:07 -0300

    fix: Ajuste no import com type.

--- sistema-consultas/src/interfaces/consulta.ts
+++ sistema-consultas/src/interfaces/consulta.ts
@@ -1,6 +1,6 @@
-import { Medico } from "./medico";
-import { Paciente } from "../types/paciente";
-import { StatusConsulta } from "../types/statusConsulta";
+import type { Medico } from "./medico";
+import type { Paciente } from "../types/paciente";
+import type { StatusConsulta } from "../types/statusConsulta";
 export interface Consulta {
   id: number;
   medico: Medico;
--- sistema-consultas/src/interfaces/medico.ts
+++ sistema-consultas/src/interfaces/medico.ts
@@ -1,4 +1,4 @@
-import { Especialidade } from "../types/especialidade";
+import type { Especialidade } from "../types/especialidade";
 export interface Medico {
   id: number;
   nome: string;
